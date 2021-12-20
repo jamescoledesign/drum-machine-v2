ab6ce5871fa5d6eb23a15f99903b282d9b8ddc3a
James Cole <james@openstack.org>
2021-12-20 12:59:00 -0600

    Add all sounds
    Signed-off-by: James Cole <james@openstack.org>

--- src/sounds.js
+++ src/sounds.js
@@ -6,6 +6,14 @@ import HatClosed from "./sounds/drum/Hat-Closed.wav"
 import HatOpen from "./sounds/drum/Hat-Open.wav"
 import HatPedal from "./sounds/drum/Hat-Pedal.wav"
 import Kick from "./sounds/drum/Kick.wav"
+import RideBell from "./sounds/drum/Ride-Bell.wav"
+import Ride from "./sounds/drum/Ride.wav"
+import SnareCenter from "./sounds/drum/Snare-Center.wav"
+import SnareEdge from "./sounds/drum/Snare-Edge.wav"
+import SnareRim from "./sounds/drum/Snare-Rim.wav"
+import TamA from "./sounds/drum/Tam-A.wav"
+import TamB from "./sounds/drum/Tam-B.wav"
+import Tom from "./sounds/drum/Tom.wav"
 
 import Trill from "./sounds/cat/1-Trill.wav"
 import Maow from "./sounds/cat/2-Maow.wav"
@@ -15,8 +23,14 @@ import Ahh from "./sounds/cat/5-Ahh.wav"
 import Wowww from "./sounds/cat/6-Wowww.wav"
 import Uhwuhh from "./sounds/cat/7-Uhwuhh.wav"
 import Waaah from "./sounds/cat/8-Waaah.wav"
-
-
+import Meeahh from "./sounds/cat/9-Meeahh.wav"
+import Whirr from "./sounds/cat/10-Whirr.wav"
+import Ehhw from "./sounds/cat/11-Ehhw.wav"
+import Mmreaww from "./sounds/cat/12-Mmreaww.wav"
+import Mmyeah from "./sounds/cat/13-Mmyeah.wav"
+import Mehh from "./sounds/cat/14-Mehh.wav"
+import Mahh from "./sounds/cat/15-Mahh.wav"
+import MowMeowRow from "./sounds/cat/16-MowMeowRow.wav"
 
 export const sounds = {
   "drumSounds": [
@@ -27,7 +41,15 @@ export const sounds = {
       HatClosed,
       HatOpen,
       HatPedal,
-      Kick
+      Kick,
+      RideBell,
+      Ride,
+      SnareCenter,
+      SnareEdge,
+      SnareRim,
+      TamA,
+      TamB,
+      Tom
     ],
     "catSounds": [
       Trill,
@@ -37,7 +59,15 @@ export const sounds = {
       Ahh,
       Wowww,
       Uhwuhh,
-      Waaah
+      Waaah,
+      Meeahh,
+      Whirr,
+      Ehhw,
+      Mmreaww,
+      Mmyeah,
+      Mehh,
+      Mahh,
+      MowMeowRow
     ]
 }
 
