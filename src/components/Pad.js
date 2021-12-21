5b14662c317353728b05de2f566563f64b7c8e34
James Cole <james@openstack.org>
2021-12-21 11:50:13 -0600

    Add keyboard accessibility
    Signed-off-by: James Cole <james@openstack.org>

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -40,23 +40,63 @@ function Pad(props) {
     }
 
     document.onkeydown = function(e) {
-        let keyResult = "pad" + e.key;
-        let pad = document.getElementById(keyResult);
-{/* This is where im stuck */} console.log(pad.id);
-        padSound = makeSound(props.mode, 0);       
+        let keyResult = e.key.toLowerCase();
+        let pad = document.getElementById("pad" + keyResult);
+        let newKey;
+        switch (keyResult) {
+            case "q":
+                newKey = "4";
+            break;
+            case "w":
+                newKey = "5";
+            break;
+            case "e":
+                newKey = "6";
+            break;
+            case "r":
+                newKey = "7";
+            break;
+            case "a":
+                newKey = "8";
+            break;
+            case "s":
+                newKey = "9";
+            break;
+            case "d":
+                newKey = "10";
+            break;
+            case "f":
+                newKey = "11";
+            break;
+            case "z":
+                newKey = "12";
+            break;
+            case "x":
+                newKey = "13";
+            break;
+            case "c":
+                newKey = "14";
+            break;
+            case "v":
+                newKey = "15";
+            break;
+            default:
+                newKey = keyResult - 1;
+        }
+        padSound = makeSound(props.mode, newKey);       
         pad.style.backgroundColor = "red";
         padSound.play();
     }
 
     document.onkeyup = function(e) {
-        let keyResult = "pad" + e.key;
-        let pad = document.getElementById(keyResult);
+        let keyResult = e.key.toLowerCase();
+        let pad = document.getElementById("pad" + keyResult);
         pad.style.backgroundColor = "#212121";
         padSound.pause();
         padSound.currentTime = 0;
     }
 
-    return (
+        return (
         <div id={props.id} className="pad" onMouseDown={handleClick} onMouseUp={resetPad} sound={props.sound} mode={props.mode}>
             <div className="pad-content noselect">
                 <div className="sound-title">{padName}</div>
